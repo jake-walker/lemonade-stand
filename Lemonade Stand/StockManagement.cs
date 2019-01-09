e679bf51494ba1157185a9d881ca8cabad3b8f67
Jake Walker <hi@jakew.me>
2019-01-09 11:39:33 +0000

    Initial Project and Classes

---
+++ Lemonade Stand/StockManagement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemonade_Stand
+{
+    class StockManagement
+    {
+        public Dictionary<Product, StockInformation> Stock { get; set; }
+    }
+}
